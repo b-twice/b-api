822c78f3306a85d362d7757b0db158d74d785526
Brian Brown <brbrowngeo@gmail.com>
2019-09-11 06:18:53 -0400

    attempt to embed weather credential

--- api/controllers/weather/Weather.Controller.cs
+++ api/controllers/weather/Weather.Controller.cs
@@ -1,32 +1,32 @@
-// using System.Threading.Tasks;
-// using DarkSky.Models;
-// using DarkSky.Services;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using DarkSky.Models;
+using DarkSky.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-// namespace Budget.API 
-// {
+namespace Budget.API 
+{
 
-//     [Route("Weather")]
-//     [ApiController]
-//     [ApiConventionType(typeof(DefaultApiConventions))]
-//     public class WeatherController: ControllerBase
-//     {
-//         private readonly ILogger _logger;
-//         private readonly DarkSkyService _darkSkyService;
-//         public WeatherController(ILogger<WeatherController> logger, DarkSkyService darkSkyService)
-//         {
-//            _logger = logger;
-//           _darkSkyService = darkSkyService;
-//         }
+    [Route("Weather")]
+    [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    public class WeatherController: ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly DarkSkyService _darkSkyService;
+        public WeatherController(ILogger<WeatherController> logger, DarkSkyService darkSkyService)
+        {
+           _logger = logger;
+          _darkSkyService = darkSkyService;
+        }
 
-//         [HttpGet("Forecast")]
-//         [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Find))]
-//         public async Task<ActionResult<DarkSkyResponse>> GetForecast() 
-//         {
-//             return await _darkSkyService.GetForecast(42.915, -78.741);
+        [HttpGet("Forecast")]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Find))]
+        public async Task<ActionResult<DarkSkyResponse>> GetForecast() 
+        {
+            return await _darkSkyService.GetForecast(42.915, -78.741);
 
-//         }
+        }
 
-//    }
-// }
+   }
+}
