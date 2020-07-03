7bc02801bed10d0807d8ef83c9aae85b1f552a33
Brian Brown <brian.brown@udig.com>
2020-07-03 10:40:18 -0400

    secure routes, fix object reference

--- Models/Extensions/ApiDbContextPartial.cs
+++ Models/Extensions/ApiDbContextPartial.cs
@@ -8,15 +8,15 @@ namespace B.API.Models {
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
         {
             // prevent self referential loops
-            modelBuilder.Entity<PostGroup>().Ignore(c => c.Post);
-            modelBuilder.Entity<BookAuthor>().Ignore(c => c.Book);
-            modelBuilder.Entity<BookStatus>().Ignore(c => c.Book);
-            modelBuilder.Entity<BookCategory>().Ignore(c => c.Book);
-            modelBuilder.Entity<User>().Ignore(c => c.TransactionRecord);
-            modelBuilder.Entity<Bank>().Ignore(c => c.TransactionRecord);
-            modelBuilder.Entity<TransactionCategory>().Ignore(c => c.TransactionRecord);
-            modelBuilder.Entity<TransactionRecordTag>().Ignore(c => c.TransactionRecord);
-            modelBuilder.Entity<TransactionTag>().Ignore(c => c.TransactionRecordTag);
+            // modelBuilder.Entity<PostGroup>().Ignore(c => c.Post);
+            // modelBuilder.Entity<BookAuthor>().Ignore(c => c.Book);
+            // modelBuilder.Entity<BookStatus>().Ignore(c => c.Book);
+            // modelBuilder.Entity<BookCategory>().Ignore(c => c.Book);
+            // modelBuilder.Entity<User>().Ignore(c => c.TransactionRecord);
+            // modelBuilder.Entity<Bank>().Ignore(c => c.TransactionRecord);
+            // modelBuilder.Entity<TransactionCategory>().Ignore(c => c.TransactionRecord);
+            // modelBuilder.Entity<TransactionRecordTag>().Ignore(c => c.TransactionRecord);
+            // modelBuilder.Entity<TransactionTag>().Ignore(c => c.TransactionRecordTag);
 
 
             modelBuilder.Entity<TransactionRecord>().Property(t => t.Amount).HasColumnType("decimal");
