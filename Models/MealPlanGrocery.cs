582fc4a7372a7af42010620388588cda023b6456
brianbrowndev <brbrowngeo@gmail.com>
2022-01-13 20:40:39 -0500

    4.0.4 - nullable field bug fix

--- Models/MealPlanGrocery.cs
+++ Models/MealPlanGrocery.cs
@@ -9,8 +9,8 @@ namespace B.API.Models
         public long? MealPlanId { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
-        public decimal Count { get; set; }
-        public decimal Weight { get; set; }
+        public decimal? Count { get; set; }
+        public decimal? Weight { get; set; }
         public string Unit { get; set; }
         public long? Dirty { get; set; }
         public string Supermarket { get; set; }
